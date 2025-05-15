c7dfae82f9a56f479cf04c43de47f04d99f9abf9
Menke <25MenkeL@badinhs.org>
2025-05-15 13:05:27 -0400

    boilerplate

---
+++ challenges.js
@@ -0,0 +1,16 @@
+// set 1 number 2
+
+function squareDiff(n, numbers) {
+    let mainDiagonal = 0;
+    let otherDiagonal = 0; 
+
+    for (let i = 0;i<n;i++) {
+        mainDiagonal += numbers[i*n+i];
+        otherDiagonal += numbers[i*n+(n-1-i)];
+    }
+
+    let difference = Math.abs(mainDiagonal-otherDiagonal);
+    return difference;
+}
+
+console.log(squareDiff(3,[11,2,4,4,5,6,10,8,-12]));
