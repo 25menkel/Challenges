2847fbd34ef7da148d8ce5e4c3f28b8fc527a1a3
Menke <25MenkeL@badinhs.org>
2025-05-20 09:07:02 -0400

    fixed i problem and completed set 2 challenge 2

--- challenge4.js
+++ challenge4.js
@@ -1,10 +1,19 @@
 // set 2 challenge 2
 
 function isDivisibleByAll(number,max){
-    for(leti=1;i<=max;i++){
+    for(let i = 1; i <= max; i++){
         if(number% i!==0){
             return false;
         }
     }
     return true;
+}
+
+let num = 20;
+while(true){
+    if(isDivisibleByAll(num,20)){
+        console.log(num);
+        break;
+    }
+    num++;
 }
